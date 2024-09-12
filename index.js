72ae1cf4794ff406bc6c9e15711f8fb5050bc659
Santino <srossiprieto@gmail.com>
2024-09-12 15:51:55 -0300

    feat: index

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Importa el middleware CORS
-import userRoutes from './routes/users.routes.js';
-import connectDB from './models/db.js'; // Importa la función de conexión a la base de datos
+import cors from 'cors';
+import userRoutes from './src/routes/users.routes.js';
+import connectDB from './src/models/db.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.DB_PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Conectar a la base de datos
 connectDB();
 
 // Middlewares
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-// Routes
+// Rutas
 app.use('/api/users', userRoutes);
 
-// Start server
+// Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
